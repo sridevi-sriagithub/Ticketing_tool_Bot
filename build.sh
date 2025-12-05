c90f5e7f3bb5a2d7dea2ecb0df3473ba22b85b6b
sridevi-sriagithub <sridevi@sriainfotech.com>
2025-12-05 16:44:52 +0530

    Add build script for Render

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
+# pip install -r requirements.txt
+# pip install botbuilder-core==4.14.0 botbuilder-schema==4.14.0 botbuilder-dialogs==4.14.0 aiohttp --no-deps
+
+
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
-pip install botbuilder-core==4.14.0 botbuilder-schema==4.14.0 botbuilder-dialogs==4.14.0 aiohttp --no-deps
+
+echo "Installing Bot Framework packages..."
+pip install botbuilder-core==4.14.0 \
+           botbuilder-schema==4.14.0 \
+           botbuilder-dialogs==4.14.0 \
+           aiohttp \
+           msrest --no-deps
+
+echo "Build completed!"
